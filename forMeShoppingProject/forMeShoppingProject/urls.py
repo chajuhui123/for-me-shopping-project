3cc99fdfd798eeb023bea528a802f0dd93628b58
Juhee(Joy) <chajuhui123@naver.com>
2021-06-29 14:40:05 +0900

    feat : Add user app

--- forMeShoppingProject/forMeShoppingProject/urls.py
+++ forMeShoppingProject/forMeShoppingProject/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from mainApp.views import * #mainApp의 views에서 모든 함수 불러옴
+from userApp.views import * #userApp의 views에서 모든 함수 불러옴
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name = "main"),
+    path('signup/', signup_view, name = "signup"),
+    path('login/', login_view, name = "login"), 
+    path('logout/', logout_view, name = "logout"),
 ]
---
+++ forMeShoppingProject/userApp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+from .models import CustomUser
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password1', 'password2', 'length', 'shoulder', 'chest', 'waist', 'rise', 'hem']
