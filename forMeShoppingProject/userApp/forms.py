73fd32410a62e4dd947d590e815efe19462a98ac
ylim2548 <stella2548@naver.com>
2021-06-30 13:10:47 +0900

    adding signup page...

--- forMeShoppingProject/userApp/forms.py
+++ forMeShoppingProject/userApp/forms.py
@@ -1,9 +1,31 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import fields
+from django.forms import fields, TextInput, PasswordInput
 from .models import CustomUser
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'password1', 'password2', 'length', 'shoulder', 'chest', 'waist', 'rise', 'hem']
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'style': 'width: 150px; height: 20px',
+                'placeholder': '아이디 입력'
+            }),
+            'password1': PasswordInput(attrs={
+                'class': 'form-control',
+                'style': 'width: 150px; height: 20px',
+                'placeholder': '비밀번호'
+            }),
+            'password2': PasswordInput(attrs={
+                'class': 'form-control',
+                'style': 'width: 150px; height: 20px',
+                'placeholder': '비밀번호 확인'
+            }),
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'style': 'width: 150px; height: 20px',
+                'placeholder': '아이디 입력'
+            }),
+        }
