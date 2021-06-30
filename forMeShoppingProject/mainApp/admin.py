152195a232cf51c8c3f2a03830fd3ee72748eb44
hanjinsu97 <hanjinsu97@gmail.com>
2021-06-30 13:11:35 +0900

    Navbarfooter

--- forMeShoppingProject/mainApp/admin.py
+++ forMeShoppingProject/mainApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Item, Measure
+from .models import Item, Measure,FittingModel,Store
 
 # Register your models here.
 
 admin.site.register(Item), 
 admin.site.register(Measure),
+
+admin.site.register(FittingModel),
+admin.site.register(Store),
