b3b61292febf2d5294b524b9ae08012ab9c9c676
ylim2548 <stella2548@naver.com>
2021-06-30 15:25:11 +0900

    again Item model modify

--- forMeShoppingProject/forMeShoppingProject/settings.py
+++ forMeShoppingProject/forMeShoppingProject/settings.py
@@ -133,3 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- forMeShoppingProject/forMeShoppingProject/urls.py
+++ forMeShoppingProject/forMeShoppingProject/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from mainApp.views import * #mainApp의 views에서 모든 함수 불러옴
 from userApp.views import * #userApp의 views에서 모든 함수 불러옴
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name = "main"),
     path('signup/', signup_view, name = "signup"),
     path('login/', login_view, name = "login"), 
     path('logout/', logout_view, name = "logout"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
